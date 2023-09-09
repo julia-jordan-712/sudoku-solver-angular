86d0d3712416c31828635cd98438825dbeedc5cc
julia-jordan-712 <julia.jordan712@googlemail.com>
2023-09-09 14:16:13 +0200

    Add more sudokus

--- src/app/components/sudoku-settings/services/sudoku-dropdown-selection.service.ts
+++ src/app/components/sudoku-settings/services/sudoku-dropdown-selection.service.ts
@@ -2,6 +2,10 @@ import { Injectable } from '@angular/core';
 import { DropdownInputOption } from '@app/components/input-field/dropdown-input/dropdown-input.component';
 import { SudokuGrid } from '@app/shared/types/sudoku-grid';
 import { Puzzle4x4 } from '@app/test/puzzles/puzzle-4x4';
+import { PuzzleAdvanced } from '@app/test/puzzles/puzzle-advanced';
+import { PuzzleExtreme } from '@app/test/puzzles/puzzle-extreme';
+import { PuzzleHard } from '@app/test/puzzles/puzzle-hard';
+import { PuzzleMedium } from '@app/test/puzzles/puzzle-medium';
 import { PuzzleSimple } from '@app/test/puzzles/puzzle-simple';
 import { TranslateService } from '@ngx-translate/core';
 
@@ -39,6 +43,26 @@ export class SudokuDropdownSelectionService {
         grid: Puzzle4x4.EMPTY_SQUARE,
       },
       {
+        id: 'Puzzle4x4.COMPLETE',
+        name: this.translate.stream('PUZZLE.4x4.COMPLETE'),
+        grid: Puzzle4x4.COMPLETE,
+      },
+      {
+        id: 'Puzzle4x4.INCOMPLETE_INVALID_COLUMN',
+        name: this.translate.stream('PUZZLE.4x4.INCOMPLETE_INVALID_COLUMN'),
+        grid: Puzzle4x4.INCOMPLETE_INVALID_COLUMN,
+      },
+      {
+        id: 'Puzzle4x4.INCOMPLETE_INVALID_ROW',
+        name: this.translate.stream('PUZZLE.4x4.INCOMPLETE_INVALID_ROW'),
+        grid: Puzzle4x4.INCOMPLETE_INVALID_ROW,
+      },
+      {
+        id: 'Puzzle4x4.INCOMPLETE_INVALID_SQUARE',
+        name: this.translate.stream('PUZZLE.4x4.INCOMPLETE_INVALID_SQUARE'),
+        grid: Puzzle4x4.INCOMPLETE_INVALID_SQUARE,
+      },
+      {
         id: 'PuzzleSimple.PUZZLE_1.puzzle',
         name: this.translate.stream('PUZZLE.SIMPLE.1.PUZZLE'),
         grid: PuzzleSimple.PUZZLE_1.puzzle,
@@ -47,6 +71,46 @@ export class SudokuDropdownSelectionService {
         id: 'PuzzleSimple.PUZZLE_1.solution',
         name: this.translate.stream('PUZZLE.SIMPLE.1.SOLVED'),
         grid: PuzzleSimple.PUZZLE_1.solution,
+      },
+      {
+        id: 'PuzzleMedium.PUZZLE_1.puzzle',
+        name: this.translate.stream('PUZZLE.MEDIUM.1.PUZZLE'),
+        grid: PuzzleMedium.PUZZLE_1.puzzle,
+      },
+      {
+        id: 'PuzzleMedium.PUZZLE_1.solution',
+        name: this.translate.stream('PUZZLE.MEDIUM.1.SOLVED'),
+        grid: PuzzleMedium.PUZZLE_1.solution,
+      },
+      {
+        id: 'PuzzleAdvanced.PUZZLE_1.puzzle',
+        name: this.translate.stream('PUZZLE.ADVANCED.1.PUZZLE'),
+        grid: PuzzleAdvanced.PUZZLE_1.puzzle,
+      },
+      {
+        id: 'PuzzleAdvanced.PUZZLE_1.solution',
+        name: this.translate.stream('PUZZLE.ADVANCED.1.SOLVED'),
+        grid: PuzzleAdvanced.PUZZLE_1.solution,
+      },
+      {
+        id: 'PuzzleHard.PUZZLE_1.puzzle',
+        name: this.translate.stream('PUZZLE.HARD.1.PUZZLE'),
+        grid: PuzzleHard.PUZZLE_1.puzzle,
+      },
+      {
+        id: 'PuzzleHard.PUZZLE_1.solution',
+        name: this.translate.stream('PUZZLE.HARD.1.SOLVED'),
+        grid: PuzzleHard.PUZZLE_1.solution,
+      },
+      {
+        id: 'PuzzleExtreme.PUZZLE_1.puzzle',
+        name: this.translate.stream('PUZZLE.EXTREME.1.PUZZLE'),
+        grid: PuzzleExtreme.PUZZLE_1.puzzle,
+      },
+      {
+        id: 'PuzzleExtreme.PUZZLE_1.solution',
+        name: this.translate.stream('PUZZLE.EXTREME.1.SOLVED'),
+        grid: PuzzleExtreme.PUZZLE_1.solution,
       },
     ];
   }
---
+++ src/app/test/puzzles/puzzle-advanced.ts
@@ -0,0 +1,28 @@
+import { TestPuzzle } from '@app/test/puzzles/test-puzzle';
+
+export class PuzzleAdvanced {
+  public static readonly PUZZLE_1: TestPuzzle = {
+    puzzle: [
+      [1,undefined,undefined,undefined,undefined,8,undefined,undefined,9],
+      [undefined,undefined,2,undefined,undefined,undefined,undefined,undefined,8],
+      [undefined,8,undefined,5,4,9,undefined,undefined,undefined],
+      [undefined,4,undefined,2,undefined,undefined,9,undefined,undefined],
+      [3,undefined,9,undefined,undefined,undefined,2,undefined,1],
+      [undefined,undefined,1,undefined,undefined,5,undefined,4,undefined],
+      [undefined,undefined,undefined,9,1,2,undefined,3,undefined],
+      [7,undefined,undefined,undefined,undefined,undefined,1,undefined,undefined],
+      [2,undefined,undefined,7,undefined,undefined,undefined,undefined,6],
+    ],
+    solution: [
+      [1,3,4,6,2,8,5,7,9],
+      [9,5,2,1,3,7,4,6,8],
+      [6,8,7,5,4,9,3,1,2],
+      [5,4,6,2,7,1,9,8,3],
+      [3,7,9,4,8,6,2,5,1],
+      [8,2,1,3,9,5,6,4,7],
+      [4,6,8,9,1,2,7,3,5],
+      [7,9,5,8,6,3,1,2,4],
+      [2,1,3,7,5,4,8,9,6],
+    ],
+  };
+}
