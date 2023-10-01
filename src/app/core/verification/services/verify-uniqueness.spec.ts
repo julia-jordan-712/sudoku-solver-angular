acae75cc31601d06aa2202b46d64cbe8627e6693
julia-jordan-712 <julia.jordan712@googlemail.com>
2023-10-01 13:18:46 +0200

    Fix: some error messages were displayed multiple times

--- src/app/core/verification/services/verify-uniqueness.spec.ts
+++ src/app/core/verification/services/verify-uniqueness.spec.ts
@@ -178,11 +178,13 @@ describe(VerifyUniqueness.name, () => {
       it(`should recognize invalid number ${invalidNumber} in a 4x4 Sudoku when tracking uniqueness`, () => {
         const grid: SudokuGrid = SudokuGridUtil.clone(Puzzle4x4.COMPLETE);
         grid[0][0] = invalidNumber;
+        grid[3][3] = invalidNumber;
 
         const result = new VerifyUniqueness(grid, 4).verify({
           trackUniquenessViolations: true,
         });
         expect(result.isValid()).toBeFalse();
+        expect(result.getErrors().length).toEqual(1);
         expect(result.getErrors()[0]).toEqual(
           VerifyI18nKey.ERROR_INVALID_NUMBERS(4),
         );
@@ -191,11 +193,13 @@ describe(VerifyUniqueness.name, () => {
       it(`should recognize invalid number ${invalidNumber} in a 4x4 Sudoku when not tracking uniqueness`, () => {
         const grid: SudokuGrid = SudokuGridUtil.clone(Puzzle4x4.COMPLETE);
         grid[0][0] = invalidNumber;
+        grid[3][3] = invalidNumber;
 
         const result = new VerifyUniqueness(grid, 4).verify({
           trackUniquenessViolations: false,
         });
         expect(result.isValid()).toBeFalse();
+        expect(result.getErrors().length).toEqual(1);
         expect(result.getErrors()[0]).toEqual(
           VerifyI18nKey.ERROR_INVALID_NUMBERS(4),
         );
--- src/app/shared/types/i18n-key.ts
+++ src/app/shared/types/i18n-key.ts
@@ -2,3 +2,23 @@ export interface I18nKey {
   key: string;
   params?: { [parameterName: string]: string | number | boolean };
 }
+
+export function i18nKeyEqual(key1: I18nKey, key2: I18nKey): boolean {
+  if (key1.key === key2.key) {
+    if (key1.params == undefined && key2.params == undefined) {
+      return true;
+    }
+    if (key1.params != undefined && key2.params != undefined) {
+      if (Object.keys(key1.params).length === Object.keys(key2.params).length) {
+        let sameParameters = true;
+        const paramKeys = Object.keys(key1.params);
+        for (let i = 0; i < paramKeys.length && sameParameters; i++) {
+          sameParameters =
+            key1.params[paramKeys[i]] === key2.params[paramKeys[i]];
+        }
+        return sameParameters;
+      }
+    }
+  }
+  return false;
+}
