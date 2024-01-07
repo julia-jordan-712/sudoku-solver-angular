6d828bee789402b400470cbe1c63883dc43841a9
julia-jordan-712 <julia.jordan712@googlemail.com>
2024-01-07 18:39:46 +0100

    Unit tests for elimination from row/column/square

--- src/app/core/solver/solver-eliminate/solver-eliminate-util.ts
+++ src/app/core/solver/solver-eliminate/solver-eliminate-util.ts
@@ -30,4 +30,152 @@ export class SolverEliminateUtil {
       false,
     );
   }
+
+  static commonTestData(): {
+    title: string;
+    input: (number | number[])[][];
+    expected: (number | number[])[][];
+    changed: boolean;
+  }[] {
+    const all = [1, 2, 3, 4];
+
+    return [
+      {
+        title:
+          "should remove 3 from second row in second square if only 1 and 2 are available in first row of first square",
+        input: [
+          [[1, 2], [1, 2], all, all],
+          [all, all, all, all],
+          [all, all, all, all],
+          [all, all, all, all],
+        ],
+        expected: [
+          [[1, 2], [1, 2], all, all],
+          [all, all, [1, 2, 4], [1, 2, 4]],
+          [all, all, all, all],
+          [all, all, all, all],
+        ],
+        changed: true,
+      },
+      {
+        title:
+          "should remove 1 from second row in first square if it is not available in first row of second square",
+        input: [
+          [all, all, [2, 3], [3, 4]],
+          [all, all, all, all],
+          [all, all, all, all],
+          [all, all, all, all],
+        ],
+        expected: [
+          [all, all, [2, 3], [3, 4]],
+          [[2, 3, 4], [2, 3, 4], all, all],
+          [all, all, all, all],
+          [all, all, all, all],
+        ],
+        changed: true,
+      },
+      {
+        title:
+          "should remove 3 from first row in second square if only 1 and 2 are available in second row of first square",
+        input: [
+          [all, all, all, all],
+          [[1, 2], [1, 2], all, all],
+          [all, all, all, all],
+          [all, all, all, all],
+        ],
+        expected: [
+          [all, all, [1, 2, 4], [1, 2, 4]],
+          [[1, 2], [1, 2], all, all],
+          [all, all, all, all],
+          [all, all, all, all],
+        ],
+        changed: true,
+      },
+      {
+        title:
+          "should remove 1 from first row in first square if it is not available in second row of second square",
+        input: [
+          [all, all, all, all],
+          [all, all, [2, 3], [3, 4]],
+          [all, all, all, all],
+          [all, all, all, all],
+        ],
+        expected: [
+          [[2, 3, 4], [2, 3, 4], all, all],
+          [all, all, [2, 3], [3, 4]],
+          [all, all, all, all],
+          [all, all, all, all],
+        ],
+        changed: true,
+      },
+      {
+        title:
+          "should remove 3 from second column in third square if only 1,2,4 are available in first column of first square",
+        input: [
+          [[1, 2], all, all, all],
+          [[1, 4], all, all, all],
+          [all, all, all, all],
+          [all, all, all, all],
+        ],
+        expected: [
+          [[1, 2], all, all, all],
+          [[1, 4], all, all, all],
+          [all, [1, 2, 4], all, all],
+          [all, [1, 2, 4], all, all],
+        ],
+        changed: true,
+      },
+      {
+        title:
+          "should remove 4 from third column in second square if only 1,2,3 are available in second column of fourth square",
+        input: [
+          [all, all, all, all],
+          [all, all, all, all],
+          [all, all, all, [1, 2, 3]],
+          [all, all, all, [1]],
+        ],
+        expected: [
+          [all, all, [1, 2, 3], all],
+          [all, all, [1, 2, 3], all],
+          [all, all, all, [1, 2, 3]],
+          [all, all, all, [1]],
+        ],
+        changed: true,
+      },
+      {
+        title:
+          "should not remove value from squares if it is possible in same row of multiple squares",
+        input: [
+          [all, [1, 2], [1, 2], all],
+          [all, all, all, all],
+          [all, all, all, all],
+          [all, all, all, all],
+        ],
+        expected: [
+          [all, [1, 2], [1, 2], all],
+          [all, all, all, all],
+          [all, all, all, all],
+          [all, all, all, all],
+        ],
+        changed: false,
+      },
+      {
+        title:
+          "should not remove value from squares if it is possible in same column of multiple squares",
+        input: [
+          [all, all, all, all],
+          [[1, 2, 3], all, all, all],
+          [[1, 3], all, all, all],
+          [all, all, all, all],
+        ],
+        expected: [
+          [all, all, all, all],
+          [[1, 2, 3], all, all, all],
+          [[1, 3], all, all, all],
+          [all, all, all, all],
+        ],
+        changed: false,
+      },
+    ];
+  }
 }
