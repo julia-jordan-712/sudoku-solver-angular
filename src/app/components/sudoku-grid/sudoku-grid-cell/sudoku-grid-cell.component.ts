67959edbfd175f74c1a6ad17afb3e6578bb64482
julia-jordan-712 <julia.jordan712@googlemail.com>
2024-03-29 15:47:00 +0100

    Switch between previous and current value in sudoku-grid-cell

--- src/app/components/sudoku-grid/sudoku-grid-cell/sudoku-grid-cell.component.ts
+++ src/app/components/sudoku-grid/sudoku-grid-cell/sudoku-grid-cell.component.ts
@@ -2,6 +2,7 @@ import {
   Component,
   EventEmitter,
   HostBinding,
+  HostListener,
   Input,
   OnChanges,
   Output,
@@ -10,6 +11,7 @@ import {
 import { Nullable } from "@app/shared/types/nullable";
 import { SudokuGridCell } from "@app/shared/types/sudoku-grid";
 import { isArray, isNotArray } from "@app/shared/util/is-array";
+import { BehaviorSubject } from "rxjs";
 
 @Component({
   selector: "app-sudoku-grid-cell",
@@ -17,14 +19,23 @@ import { isArray, isNotArray } from "@app/shared/util/is-array";
   styleUrls: ["./sudoku-grid-cell.component.scss"],
 })
 export class SudokuGridCellComponent implements OnChanges {
-  value: Nullable<number>;
-  values: Nullable<number[]>;
-  size = 32;
+  private value: Nullable<number>;
+  private values: Nullable<number[]>;
+  private previousValue: Nullable<number>;
+  private previousValues: Nullable<number[]>;
+  displayValue$: BehaviorSubject<Nullable<number>> = new BehaviorSubject<
+    Nullable<number>
+  >(null);
+  displayValues$: BehaviorSubject<Nullable<number[]>> = new BehaviorSubject<
+    Nullable<number[]>
+  >(null);
 
   @Input({ required: true })
   set cell(cell: SudokuGridCell) {
     this.setCell(cell);
   }
+
+  size = 32;
 
   @Input({ required: true })
   maxValue = 1;
@@ -68,6 +79,9 @@ export class SudokuGridCellComponent implements OnChanges {
   }
 
   private setCell(cell: SudokuGridCell): void {
+    this.previousValue = this.value;
+    this.previousValues = this.values;
+
     if (isArray(cell)) {
       this.values = cell;
       this.value = null;
@@ -75,9 +89,63 @@ export class SudokuGridCellComponent implements OnChanges {
       this.values = null;
       this.value = cell;
     }
+
+    this.displayValue$.next(this.value);
+    this.displayValues$.next(this.values);
   }
 
   onChange(value: number): void {
     this.valueChange.emit(value);
+  }
+
+  isHoveringOverCell = false;
+  timeOutFnId: Nullable<number> = null;
+
+  @HostListener("mouseenter")
+  onMouseEnter(): void {
+    if (!this.isHoveringOverCell) {
+      this.isHoveringOverCell = true;
+      this.startTimer(() => this.switchBetweenPreviousAndCurrentValue(1), 500);
+    }
+  }
+
+  private switchBetweenPreviousAndCurrentValue(counter: number): void {
+    this.stopTimer();
+    if (!this.isHoveringOverCell) {
+      return;
+    }
+    if (counter % 2 === 0) {
+      this.displayValue$.next(this.value);
+      this.displayValues$.next(this.values);
+    } else {
+      this.displayValue$.next(this.previousValue ?? this.value);
+      this.displayValues$.next(this.previousValues ?? this.values);
+    }
+    this.startTimer(
+      () => this.switchBetweenPreviousAndCurrentValue(++counter),
+      1000,
+    );
+  }
+
+  @HostListener("mouseleave")
+  onMouseLeave(): void {
+    this.isHoveringOverCell = false;
+    this.stopTimer();
+    this.displayValue$.next(this.value);
+    this.displayValues$.next(this.values);
+  }
+
+  private startTimer(callback: () => void, ms: number): void {
+    this.stopTimer();
+    if (this.isHoveringOverCell) {
+      this.timeOutFnId = setTimeout(callback, ms) as unknown as number;
+    }
+  }
+
+  private stopTimer(): void {
+    if (this.timeOutFnId) {
+      clearTimeout(this.timeOutFnId);
+      this.timeOutFnId = null;
+    }
   }
 }
