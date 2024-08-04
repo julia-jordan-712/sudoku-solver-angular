6967f8022eccb7c347a41105da184f249028f127
julia-jordan-712 <julia.jordan712@googlemail.com>
2024-08-04 17:25:40 +0200

    Tests for SudokuGridCellValidator

--- src/app/components/sudoku-grid/sudoku-grid-cell/sudoku-grid-cell.validator.spec.ts
+++ src/app/components/sudoku-grid/sudoku-grid-cell/sudoku-grid-cell.validator.spec.ts
@@ -1,7 +1,77 @@
 import { SudokuGridCellValidator } from "@app/components/sudoku-grid/sudoku-grid-cell/sudoku-grid-cell.validator";
+import { Nullable } from "@app/shared/types/nullable";
 
 describe(SudokuGridCellValidator.name, () => {
-  it("should create an instance", () => {
-    expect(new SudokuGridCellValidator()).toBeTruthy();
+  describe("max value 10", () => {
+    [
+      { value: null, valid: true },
+      { value: undefined, valid: true },
+      { value: NaN, valid: false },
+      { value: Number.MIN_SAFE_INTEGER, valid: false },
+      { value: -1, valid: false },
+      { value: 0, valid: false },
+      { value: 1, valid: true },
+      { value: 9, valid: true },
+      { value: 10, valid: true },
+      { value: 11, valid: false },
+    ].forEach((params) => {
+      executeTest(params.valid, params.value, 10);
+    });
+  });
+
+  describe("max value 1", () => {
+    [
+      { value: null, valid: true },
+      { value: undefined, valid: true },
+      { value: NaN, valid: false },
+      { value: Number.MIN_SAFE_INTEGER, valid: false },
+      { value: -1, valid: false },
+      { value: 0, valid: false },
+      { value: 1, valid: true },
+      { value: 2, valid: false },
+    ].forEach((params) => {
+      executeTest(params.valid, params.value, 1);
+    });
+  });
+
+  describe("max value 0", () => {
+    [
+      { value: null, valid: true },
+      { value: undefined, valid: true },
+      { value: NaN, valid: false },
+      { value: Number.MIN_SAFE_INTEGER, valid: false },
+      { value: -1, valid: false },
+      { value: 0, valid: false },
+      { value: 1, valid: false },
+    ].forEach((params) => {
+      executeTest(params.valid, params.value, 0);
+    });
+  });
+
+  describe("NO max value", () => {
+    [
+      { value: null, valid: true },
+      { value: undefined, valid: true },
+      { value: NaN, valid: false },
+      { value: Number.MIN_SAFE_INTEGER, valid: false },
+      { value: -1, valid: false },
+      { value: 0, valid: false },
+      { value: 1, valid: true },
+      { value: Number.MAX_SAFE_INTEGER, valid: true },
+    ].forEach((params) => {
+      executeTest(params.valid, params.value, null);
+    });
   });
 });
+
+function executeTest(
+  valid: boolean,
+  value: Nullable<number>,
+  maxValue: number | null,
+): void {
+  it(`should be ${valid ? "VALID" : "INVALID"} for value '${value}'`, () => {
+    const validator: SudokuGridCellValidator = new SudokuGridCellValidator();
+    validator.setMaxValue(maxValue);
+    expect(validator.isValid(value)).toEqual(valid);
+  });
+}
