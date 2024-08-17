762736565a6b9c6701069257a0d2ad5086d8660a
julia-jordan-712 <julia.jordan712@googlemail.com>
2024-08-17 15:23:23 +0200

    Migrate [(ngModel)]

--- src/app/components/interactions/dropdown-input/dropdown-input.component.ts
+++ src/app/components/interactions/dropdown-input/dropdown-input.component.ts
@@ -1,4 +1,11 @@
-import { Component, EventEmitter, Input, Output } from "@angular/core";
+import {
+  Component,
+  EventEmitter,
+  Input,
+  OnChanges,
+  Output,
+  SimpleChanges,
+} from "@angular/core";
 import { Nullable } from "@app/shared/types/nullable";
 import { ObjectWithId } from "@app/shared/types/object-with-id";
 import { Observable } from "rxjs";
@@ -8,7 +15,9 @@ import { Observable } from "rxjs";
   templateUrl: "./dropdown-input.component.html",
   styleUrls: ["./dropdown-input.component.scss"],
 })
-export class DropdownInputComponent<T extends DropdownInputOption> {
+export class DropdownInputComponent<T extends DropdownInputOption>
+  implements OnChanges
+{
   @Input()
   label: Nullable<string>;
 
@@ -21,7 +30,16 @@ export class DropdownInputComponent<T extends DropdownInputOption> {
   @Output()
   selected: EventEmitter<T> = new EventEmitter();
 
+  protected _selectedItem: Nullable<T>;
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes["selectedItem"]) {
+      this._selectedItem = this.selectedItem;
+    }
+  }
+
   onChange(option: T): void {
+    this._selectedItem = option;
     this.selected.emit(option);
   }
 
