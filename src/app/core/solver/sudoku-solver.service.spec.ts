5e84f4d14405be2f6fc3e259383fd3aa117a173f
julia-jordan-712 <julia.jordan712@googlemail.com>
2024-08-18 18:10:55 +0200

    Add fourth medium puzzle

--- src/app/core/solver/sudoku-solver.service.spec.ts
+++ src/app/core/solver/sudoku-solver.service.spec.ts
@@ -76,6 +76,11 @@ describe(SudokuSolverService.name, () => {
       steps: 177,
     },
     {
+      puzzle: PuzzleMedium.PUZZLE_4.puzzle,
+      name: "medium puzzle 4",
+      steps: 240,
+    },
+    {
       puzzle: PuzzleAdvanced.PUZZLE_1.puzzle,
       name: "advanced puzzle 1",
       steps: 204,
