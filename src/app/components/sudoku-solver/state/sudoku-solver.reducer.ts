596ec948941d2604f950c9ad47969013871292e9
julia-jordan-712 <julia.jordan712@googlemail.com>
2024-09-13 21:51:12 +0200

    State and initial state for SudokuSolverState

--- src/app/components/sudoku-solver/state/sudoku-solver.reducer.ts
+++ src/app/components/sudoku-solver/state/sudoku-solver.reducer.ts
@@ -1,11 +1,30 @@
 import { SudokuSolverState } from "@app/components/sudoku-solver/state/sudoku-solver.state";
 import { createFeature, createReducer } from "@ngrx/store";
+import { v4 as randomUUID } from "uuid";
 
 export class SudokuSolverReducer {
   public static readonly featureKey = "sudokuSolver";
 
   public static readonly initialState: SudokuSolverState = {
-    sudoku: undefined,
+    executionInfo: {
+      id: randomUUID(),
+      maxSteps: 10_000,
+      stepsExecuted: 0,
+      status: "NOT_STARTED",
+      time: { started: null, stopped: null, lastStep: null },
+    },
+    puzzle: undefined,
+    response: {
+      branches: [],
+      status: "UNKNOWN",
+      stepId: "",
+    },
+    viewInfo: {
+      amountOfBranches: 1,
+      delay: 0,
+      highlightNumber: null,
+      pauseAfterStep: null,
+    },
   };
 
   public static readonly reducer = createReducer(
--- src/app/components/sudoku-solver/state/sudoku-solver.state.ts
+++ src/app/components/sudoku-solver/state/sudoku-solver.state.ts
@@ -1,6 +1,42 @@
+import { SolverResponse } from "@app/core/solver/types/solver-response";
 import { Nullable } from "@app/shared/types/nullable";
+import { SolverExecution } from "@app/shared/types/solver-execution";
 import { SudokuGrid } from "@app/shared/types/sudoku-grid";
 
+interface SudokuSolverExecutionInfo {
+  /** Id for the current run of the solver */
+  id: string;
+  /** maximum steps to be executed to prevent endless loops */
+  maxSteps: number;
+  /** current status of running the solver */
+  status: SolverExecution;
+  /** steps which were executed so far during this run */
+  stepsExecuted: number;
+  /** information on the time required for the current run */
+  time: {
+    /** timestamp of the start of the current run */
+    started: number | null;
+    /** timestamp of the end of the current run */
+    stopped: number | null;
+    /** timestamp of the end of the last step */
+    lastStep: number | null;
+  };
+}
+
+interface SudokuSolverViewInfo {
+  /** total amount of branches that were opened since the solver started */
+  amountOfBranches: number;
+  /** delay in milliseconds between solver steps */
+  delay: number;
+  /** the number to be highlighted in the Sudoku to be solved */
+  highlightNumber: Nullable<number>;
+  /** the solver is to be paused when this step is reached */
+  pauseAfterStep: Nullable<number>;
+}
+
 export interface SudokuSolverState {
-  sudoku: Nullable<SudokuGrid>;
+  executionInfo: SudokuSolverExecutionInfo;
+  puzzle: Nullable<SudokuGrid>;
+  response: SolverResponse;
+  viewInfo: SudokuSolverViewInfo;
 }
