6187899051f439ccae74a6acf6e61f69f9371f24
julia-jordan-712 <julia.jordan712@googlemail.com>
2024-09-14 15:37:26 +0200

    Install and setup Cypress

---
+++ cypress.config.ts
@@ -0,0 +1,20 @@
+import { defineConfig } from "cypress";
+
+export default defineConfig({
+  component: {
+    devServer: {
+      framework: "angular",
+      bundler: "webpack",
+      options: {
+        projectConfig: {
+          root: "./",
+          sourceRoot: "src",
+          buildOptions: {
+            outputPath: "dist/sudoku-solver-angular",
+          },
+        },
+      },
+    },
+    specPattern: "**/*.cy.ts",
+  },
+});
