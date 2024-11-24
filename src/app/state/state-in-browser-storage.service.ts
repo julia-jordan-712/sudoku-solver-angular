acae548e6522d283fc3c1c2cf067f75732582e17
julia-jordan-712 <julia.jordan712@googlemail.com>
2024-11-24 18:34:57 +0100

    Also handle display language on page reload

--- src/app/state/state-in-browser-storage.service.ts
+++ src/app/state/state-in-browser-storage.service.ts
@@ -3,43 +3,55 @@ import { DomSanitizer } from "@angular/platform-browser";
 import { Logger } from "@app/core/log/logger";
 import { AppActions, AppState } from "@app/state/app-state";
 import { Store } from "@ngrx/store";
+import { TranslateService } from "@ngx-translate/core";
 import { take } from "rxjs";
 
 @Injectable({
   providedIn: "root",
 })
 export class StateInBrowserStorageService {
-  private readonly STORAGE_KEY = "suso_app_state";
+  private readonly STATE_STORAGE_KEY = "suso_app_state";
+  private readonly LANGUAGE_STORAGE_KEY = "suso_app_lang";
 
   private log: Logger = new Logger(StateInBrowserStorageService.name);
   private sanitizer: DomSanitizer = inject(DomSanitizer);
   private store: Store = inject(Store);
+  private translate: TranslateService = inject(TranslateService);
 
   setStateFromBrowser(): Promise<void> {
-    const state = this.getItem<AppState>(this.STORAGE_KEY);
-    this.removeItem(this.STORAGE_KEY);
+    const state = this.getAndRemoveItem<AppState>(this.STATE_STORAGE_KEY);
     if (state) {
       this.store.dispatch(AppActions.init({ state: state }));
       this.log.logInfo("State set from browser storage");
-      return Promise.resolve();
     } else {
       this.log.logInfo("Could not read state from browser storage");
-      return Promise.resolve();
     }
+
+    const language = this.getAndRemoveItem<string>(this.LANGUAGE_STORAGE_KEY);
+    if (language) {
+      this.translate.use(language);
+      this.log.logInfo("Language set from browser storage");
+    } else {
+      this.log.logInfo("Could not read language from browser storage");
+    }
+
+    return Promise.resolve();
   }
 
   storeStateInBrowser(): void {
     this.store.pipe(take(1)).subscribe((state) => {
       this.log.logInfo("Storing state in browser");
-      this.setItem(this.STORAGE_KEY, state);
+      this.setItem(this.STATE_STORAGE_KEY, state);
     });
+    this.setItem(this.LANGUAGE_STORAGE_KEY, this.translate.currentLang);
   }
 
-  private getItem<T extends object>(key: string): T | null {
+  private getAndRemoveItem<T extends object | string>(key: string): T | null {
     const fromStorage = this.sanitizer.sanitize(
       SecurityContext.NONE,
       localStorage.getItem(key),
     );
+    localStorage.removeItem(key);
     if (fromStorage) {
       return JSON.parse(fromStorage);
     } else {
@@ -47,11 +59,7 @@ export class StateInBrowserStorageService {
     }
   }
 
-  private removeItem(key: string): void {
-    localStorage.removeItem(key);
-  }
-
-  private setItem<T extends object>(key: string, obj: T): void {
+  private setItem<T extends object | string>(key: string, obj: T): void {
     localStorage.setItem(
       key,
       JSON.stringify(this.sanitizer.sanitize(SecurityContext.NONE, obj)),
