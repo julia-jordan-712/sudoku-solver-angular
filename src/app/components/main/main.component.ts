18d8f49be2691f3d901877c6706e5042ab42cd8a
julia-jordan-712 <julia.jordan712@googlemail.com>
2024-12-28 19:48:35 +0100

    Change layout of sudoku puzzle to grid layout

--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -1,8 +1,17 @@
-import { Component } from "@angular/core";
+import { Component, inject } from "@angular/core";
+import { SudokuPuzzleSelectors } from "@app/components/sudoku-puzzle/state/sudoku-puzzle.selectors";
+import { Store } from "@ngrx/store";
+import { Observable } from "rxjs";
 
 @Component({
   selector: "app-main",
   templateUrl: "./main.component.html",
   styleUrls: ["./main.component.scss"],
 })
-export class MainComponent {}
+export class MainComponent {
+  private store: Store = inject(Store);
+
+  showPuzzle$: Observable<boolean> = this.store.select(
+    SudokuPuzzleSelectors.selectIsShown,
+  );
+}
