e2de5776d78e453df4488cc9faa76709ee5b7e3d
julia-jordan-712 <julia.jordan712@googlemail.com>
2025-01-12 14:50:10 +0100

    Create component for labeled input with info icon

--- src/app/components/general/number-input/number-input.module.ts
+++ src/app/components/general/number-input/number-input.module.ts
@@ -1,12 +1,12 @@
 import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
 import { FormsModule } from "@angular/forms";
-import { IconModule } from "@app/components/general/icon/icon.module";
+import { LabelInputModule } from "@app/components/general/label-input/label-input.module";
 import { NumberInputComponent } from "@app/components/general/number-input/number-input.component";
 
 @NgModule({
   declarations: [NumberInputComponent],
-  imports: [CommonModule, IconModule, FormsModule],
+  imports: [CommonModule, FormsModule, LabelInputModule],
   exports: [NumberInputComponent],
 })
 export class NumberInputModule {}
