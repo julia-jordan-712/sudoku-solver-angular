4378d0c67e539ead4a9708eef9c7cd12194df720
julia-jordan-712 <julia.jordan712@googlemail.com>
2025-05-29 16:04:47 +0200

    Delete unused test components
    - DropdownInputTestComponent
    - NumberInputTestComponent
    - SelectionListTestComponent
    - SudokuGridTestComponent

--- src/test/components/number-input-test.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from "@angular/core";
-import { NumberInputComponent } from "@app/components/general/number-input/number-input.component";
-
-@Component({
-  selector: "app-number-input",
-  template: `{{ value }}`,
-})
-export class NumberInputTestComponent extends NumberInputComponent {}
